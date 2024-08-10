529a8c7fb8fb5bee7bc5e9fd640bbd586733895b
rkmonarch <rahulkulkarni5234@gmail.com>
2024-08-10 19:55:53 +0530

    feat: usdc balance API

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -12,3 +12,32 @@ export const getAPY = (liquidityRate: number) => {
 
   return depositAPY;
 };
+
+export enum chains {
+  "base",
+  "optimism",
+}
+
+export const getUSDCBalance = async (address: string, chain: chains) => {
+  if (chain === chains.base) {
+    try {
+      const response = await fetch(
+        `https://base.blockscout.com/api?module=account&action=tokenbalance&contractaddress=0x833589fcd6edb6e08f4c7c32d4f71b54bda02913&address=${address}`
+      );
+      const data = await response.json();
+      return data.result;
+    } catch (error) {
+      console.error(error);
+    }
+  } else if (chain === chains.optimism) {
+    try {
+      const response = await fetch(
+        `https://optimism.blockscout.com/api?module=account&action=tokenbalance&contractaddress=0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85&address=${address}`
+      );
+      const data = await response.json();
+      return data.result;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
