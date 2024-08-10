76c32bc8556f3abe1179975de23a696569c85a00
vrajdesai78 <vrajdesai78@gmail.com>
2024-08-10 21:59:12 +0530

    integrated apy on frontend

--- src/app/api/getAPY/op/route.ts
+++ src/app/api/getAPY/op/route.ts
@@ -1,32 +1,8 @@
+import { getAPYonOP } from "@/app/_actions";
 import { getAPY } from "@/helpers";
 
 export const GET = async (req: Request) => {
-  const getReservesResponse = await fetch(
-    `https://gateway.thegraph.com/api/${process.env.GRAPH_API_KEY}/subgraphs/id/DSfLz8oQBUeU5atALgUFQKMTSYV9mZAVYp4noLSXAfvb`,
-    {
-      method: "POST",
-      body: JSON.stringify({
-        query:
-          "{\n  reserves {\n    name\n    underlyingAsset\n    liquidityRate\n  }\n}",
-      }),
-    }
-  );
-
-  const { data } = await getReservesResponse.json();
-
-  const usdc = data.reserves.find((reserve: any) => {
-    return (
-      reserve.underlyingAsset === "0x0b2c639c533813f4aa9d7837caf62653d097ff85"
-    );
-  });
-
-  if (!usdc) {
-    return new Response("USDC not found", {
-      status: 404,
-    });
-  }
-
-  const apy = getAPY(usdc.liquidityRate);
+  const apy = Number(await getAPYonOP());
 
   return new Response(apy.toFixed(3), {
     status: 200,
